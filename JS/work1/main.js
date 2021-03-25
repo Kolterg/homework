e39e20bc26569ddc9e7e0918f9dd48c25e4c6476
Kolterg <mark.reta99@gmail.com>
2021-03-25 19:29:53 +0200

    Finished the first task of JS

---
+++ JS/work1/main.js
@@ -0,0 +1,288 @@
+// //1-2
+//
+// let str1 = 'hello';
+// let str2 = 'owu';
+// let str3 = 'com';
+// let str4 = 'ua';
+// let number1 = 1;
+// let number2 = 10;
+// let number3 = -999;
+// let number4 = 123;
+// let number5 = 3.14;
+// let number6 = 2.7;
+// let number7 = 16;
+// let bool1 = true;
+// let bool2 = false;
+//
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+// console.log(str4);
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+// console.log(number4);
+// console.log(number5);
+// console.log(number6);
+// console.log(number7);
+// console.log(bool1);
+// console.log(bool2);
+//
+// alert(str1);
+// // alert(str2);
+// // alert(str3);
+// // alert(str4);
+// // alert(number1);
+// // alert(number2);
+// // alert(number3);
+// // alert(number4);
+// // alert(number5);
+// // alert(number6);
+// // alert(number7);
+// // alert(bool1);
+// // alert(bool2);
+//
+// document.write('<h2>' + str1 + '</h2>');
+// document.write('<h2>' + str2 + '</h2>');
+// document.write('<h2>' + str3 + '</h2>');
+// document.write('<h2>' + str4 + '</h2>');
+// document.write('<h2>' + number1 + '</h2>');
+// document.write('<h2>' + number2 + '</h2>');
+// document.write('<h2>' + number3 + '</h2>');
+// document.write('<h2>' + number4 + '</h2>');
+// document.write('<h2>' + number5 + '</h2>');
+// document.write('<h2>' + number6 + '</h2>');
+// document.write('<h2>' + number7 + '</h2>');
+// document.write('<h2>' + bool1 + '</h2>');
+// document.write('<h2>' + bool2 + '</h2>');
+//
+// str1 = 'world';
+// str2 = 'okten';
+// str3 = 'net';
+// str4 = 'ru';
+// number1 = 2;
+// number2 = 20;
+// number3 = -1000;
+// number4 = 321;
+// number5 = 3.1415;
+// number6 = 7.2;
+// number7 = 32;
+// bool1 = false;
+// bool2 = true;
+//
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+// console.log(str4);
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+// console.log(number4);
+// console.log(number5);
+// console.log(number6);
+// console.log(number7);
+// console.log(bool1);
+// console.log(bool2);
+//
+// alert(str1);
+// // alert(str2);
+// // alert(str3);
+// // alert(str4);
+// // alert(number1);
+// // alert(number2);
+// // alert(number3);
+// // alert(number4);
+// // alert(number5);
+// // alert(number6);
+// // alert(number7);
+// // alert(bool1);
+// // alert(bool2);
+//
+// document.write('<h2>' + str1 + '</h2>');
+// document.write('<h2>' + str2 + '</h2>');
+// document.write('<h2>' + str3 + '</h2>');
+// document.write('<h2>' + str4 + '</h2>');
+// document.write('<h2>' + number1 + '</h2>');
+// document.write('<h2>' + number2 + '</h2>');
+// document.write('<h2>' + number3 + '</h2>');
+// document.write('<h2>' + number4 + '</h2>');
+// document.write('<h2>' + number5 + '</h2>');
+// document.write('<h2>' + number6 + '</h2>');
+// document.write('<h2>' + number7 + '</h2>');
+// document.write('<h2>' + bool1 + '</h2>');
+// document.write('<h2>' + bool2 + '</h2>');
+
+// //3
+//
+// const num1 = 12;
+// const num2 = 17;
+// const num3 = 22;
+// const str1 = 'tok';
+// const str2 = 'hot';
+// const str3 = 'kot';
+//
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+//
+// alert(num1);
+// alert(num2);
+// alert(num3);
+// alert(str1);
+// alert(str2);
+// alert(str3);
+//
+// document.write('<h3>' + num1 + '</h3>');
+// document.write('<h3>' + num2 + '</h3>');
+// document.write('<h3>' + num3 + '</h3>');
+// document.write('<h3>' + str1 + '</h3>');
+// document.write('<h3>' + str2 + '</h3>');
+// document.write('<h3>' + str3 + '</h3>');
+
+// //4-6
+//
+// let surname = prompt('set your surname')
+// let name = prompt('set your name')
+// let midleName = prompt('set your midle name')
+// let space = ' ';
+//
+// console.log(surname);
+// console.log(name);
+// console.log(midleName);
+//
+// // alert(surname);
+// // alert(name);
+// // alert(midleName);
+//
+// document.write('<h3>' + surname + '</h3>');
+// document.write('<h3>' + name + '</h3>');
+// document.write('<h3>' + midleName + '</h3>');
+//
+// let person = surname + space + name + space + midleName;
+//
+// console.log(person);
+// alert(person);
+// document.write('<h3>' + person + '</h3>');
+
+// //7
+//
+// let num1 = prompt('set first number');
+// let num2 = prompt('set second number');
+// let num3 = prompt('set third number');
+//
+// num1 = +num1;
+// num2 = +num2;
+// num3 = +num3;
+//
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+// //8
+//
+// let num1 = prompt('set first number');
+// let num2 = prompt('set second number');
+// let num3 = prompt('set third number');
+// let num4 = prompt('set fourth number');
+//
+// num1 = parseInt(num1);
+// num2 = parseInt(num2);
+// num3 = parseInt(num3);
+// num4 = parseInt(num4);
+//
+// let result = num1 + num2 + num3 + num4;
+// console.log(result);
+
+// //9
+//
+// let num1 = prompt('set first number');
+// let num2 = prompt('set second number');
+// let num3 = prompt('set third number');
+//
+// num1 = parseFloat(num1);
+// num2 = parseFloat(num2);
+// num3 = parseFloat(num3);
+//
+// let result = num1 + num2 + num3;
+//
+// console.log(result);
+
+// //10
+//
+// let num1 = prompt('set first number');
+// let num2 = prompt('set second number');
+// let num3 = prompt('set third number');
+//
+// num1 = Math.round(num1);
+// num2 = Math.round(num2);
+// num3 = Math.round(num3);
+//
+// let result = num1 + num2 + num3;
+//
+// console.log(result);
+
+// //11
+//
+// let num1 = prompt('set first number');
+// let num2 = prompt('set second number');
+//
+// num1 = parseInt(num1);
+// num2 = parseInt(num2);
+//
+// let result = Math.pow(num1, num2);
+//
+// console.log(result);
+
+// //12
+//
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+//
+// let typea = typeof a;
+// let typeb = typeof b;
+// let typec = typeof c;
+// let typed = typeof d;
+//
+// console.log(typea);
+// console.log(typeb);
+// console.log(typec);
+// console.log(typed);
+
+// //13
+//
+// console.log(5 < 6); // -> true
+// console.log(5 > 6); // -> false
+// console.log(5 == 6); // -> false
+// console.log(5 === 6); // -> false
+// console.log(10 == 10); // -> true
+// console.log(10 === 10); // -> true
+// console.log(10 < 10); // -> false
+// console.log(10 > 10); // -> false
+// console.log(10 != 10); // -> false
+// console.log(123 === '123'); // -> false
+// console.log(123 == '123'); // -> true
+
+// //14
+//
+// console.log(132 > 100 && 45 < 12 ); // F
+// console.log(34 > 33 && 23 < 90 ); // T
+// console.log(99 > 100 && 45 > 12 ); // F
+// console.log(132 > 100 || 45 < 12 ); // T
+// console.log(111 > 11 || 45 < 111 ); // T
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); // F
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); // T
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); // F
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); // T
+// console.log(!!'-1'); // T
+// console.log(!!-1); // T
+// console.log(!!'0'); // F
+// console.log(!!'null'); // F
+// console.log(!!'undefined'); // F
+// console.log(!!(3/'owu')); // F
+// console.log((111 > 11 || 45 < 111) ||  !!'0'); // T
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); // F
